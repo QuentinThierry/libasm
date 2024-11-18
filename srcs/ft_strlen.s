0114392aa95751eca275eae0d78fe068e004284d
QuentinThierry <qthierry2@gmail.com>
2024-11-18 19:00:43 +0100

    fix: adding return value into rax by substracting previous rdi with new rdi

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -2,12 +2,15 @@ section .text
 	global ft_strlen
 
 ft_strlen:
+	mov rax, rdi
 	loop:
 		cmp byte [rdi], 0
 		je done
 		inc rdi
 		jmp loop
 	done:
+		sub rdi, rax
+		mov rax, rdi
 		ret
 
 ; ft_strlen:
