2b4898deb098af0b013a943fcf4f71da6fd339ed
QuentinThierry <qthierry2@gmail.com>
2024-11-18 15:22:09 +0100

    feat: adding alternative faster strlen implementation

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -2,11 +2,20 @@ section .text
 	global ft_strlen
 
 ft_strlen:
-	mov rax, 0
 	loop:
-		cmp byte [rdi + rax], 0
+		cmp byte [rdi], 0
 		je done
-		inc rax
+		inc rdi
 		jmp loop
 	done:
 		ret
+
+; ft_strlen:
+; 	mov rax, 0
+; 	loop:
+; 		cmp byte [rdi + rax], 0
+; 		je done
+; 		inc rax
+; 		jmp loop
+; 	done:
+; 		ret
