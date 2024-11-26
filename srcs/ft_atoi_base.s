7e9c7fb454b401a750a9fe47d369f4cfcd83e83c
QuentinThierry <qthierry2@gmail.com>
2024-11-26 17:27:00 +0100

    feat: first version of ft_atoi_base working, missing some calling convention

--- srcs/ft_atoi_base.s
+++ srcs/ft_atoi_base.s
@@ -4,9 +4,23 @@ section .text
 extern ft_strlen
 
 
+is_white_space:
+	cmp dil, ' '
+	je .true
+	cmp dil, 8			; dil < 8 -> not whitespace
+	jl .false
+	cmp dil, 13			; dil > 13 -> not whitespace
+	jg .false
+
+	.true:
+	mov rax, 1
+	ret
+	.false:
+	mov rax, 0
+	ret
 
 ; rdi=char
-is_illegal_base_char:
+is_illegal_char:
 	cmp dil, ' '
 	je .true
 	cmp dil, '+'
@@ -14,9 +28,9 @@ is_illegal_base_char:
 	cmp dil, '-'
 	je .true
 	cmp dil, 8			; dil < 8 -> not illegal
-	je .false
+	jl .false
 	cmp dil, 13			; dil > 13 -> not illegal
-	je .false
+	jg .false
 
 	.false:
 	mov rax, 0
@@ -25,11 +39,13 @@ is_illegal_base_char:
 	mov rax, 1
 	ret
 
+; TODO: check stack alligment to 16
+
 ; rdi=base
 check_base:
-	push rbx						; save
+	push rbx						; save ; TODO: rbp
 	push r12
-	mov r12, rdi					; stack rdi ; TODO check if rsp or rbp
+	mov r12, rdi					; stack rdi
 	call ft_strlen
 	cmp rax, 1
 	jle .on_error					; size is <= 1
@@ -41,7 +57,7 @@ check_base:
 		je .end_loop1
 
 		mov dil, byte [r12 + rdx]
-		call is_illegal_base_char	; check if base has whitespace or +-
+		call is_illegal_char	; check if base has whitespace or +-
 		cmp rax, 1
 		je .on_error
 
@@ -72,22 +88,140 @@ check_base:
 	pop rbx
 	ret
 
+
+; rdi=str
+skip_white_space:
+	push r12
+	mov r12, rdi
+
+	xor rdi, rdi
+	xor rcx, rcx
+	.loop:						; for all char until not whitespace
+		mov dil, byte [r12 + rcx]
+		call is_white_space
+		cmp rax, 0					; if is not a whitespace or is \0, end loop
+		je .end_loop
+		inc rcx						; else look next char
+		jmp .loop
+
+	.end_loop:
+	pop r12
+	mov rax, rcx
+	ret
+
+; rdi=str (from stack)
+get_sign:
+	mov rdi, [rsp + 16]		; get the value in the stack (rsp + size(rip) + 8)
+	xor rcx, rcx
+	mov rax, 1
+
+	.loop:
+		mov dl, byte [rdi + rcx]
+		cmp dl, '+'
+		je .plus
+		cmp dl, '-'
+		je .minus
+		jmp .end_loop		; else end loop
+
+		.plus:				; on +, inc and loop
+		inc rcx
+		jmp .loop
+		.minus:				; on -, inc, negate and loop
+		inc rcx
+		neg rax
+		jmp .loop
+	
+	.end_loop
+	add [rsp + 16], rcx		; increment the value of the ptr in the stack
+	ret
+
+; rdi=base, rsi=char
+is_in_base:
+	mov rax, 0
+	.loop:
+		mov dl, byte [rdi + rax]
+		cmp dl, 0			; if end of base then char is not in base, ret -1
+		je .end_loop_false
+
+		cmp dl, sil			; if base char is the wanted char, ret rax
+		je .end_loop_true
+
+		inc rax				; else inc to check next base char
+		jmp .loop
+	.end_loop_true:
+	ret
+	.end_loop_false:
+	mov rax, -1
+	ret
+
+; rdi=str, rsi=base, rdx=sign
+get_value:
+	push rbx
+	xor rcx, rcx		; rcx = return value (TODO see calling convention ?)
+	mov r8, rdi			; r8 = str
+	mov r9, rsi			; r9 = base
+	mov r10, rdx		; r10 = sign
+	
+	mov rdi, r9			; call ft_strlen(base)
+	call ft_strlen
+	mov rbx, rax		; rbx = base size
+
+	.loop:
+		cmp byte [r8], 0
+		je .end_loop
+
+		mov rdi, r9
+		mov rsi, [r8]
+		call is_in_base
+		cmp rax, -1		; if is not in base, stop here
+		je .end_loop
+
+		imul rcx, rbx	; mult by the base size
+		add rcx, rax	; add the position of the char
+
+		inc r8
+		jmp .loop
+
+	.end_loop:
+	mov rax, rcx
+	pop rbx
+	ret
+
+
 ; entry point
 ; rdi=str, rsi=base
 ft_atoi_base:
-	push rbp			; save base pointer
+	push rbp					; save base pointer
+	push r12
 	mov rbp, rsp
 
 	mov rax, rdi
-	and rax, rsi		; AND both pointer
-	cmp rax, 0			; compare AND res to 0
-	je .fast_return		; if a ptr is NULL, error
+	and rax, rsi				; AND both pointer
+	cmp rax, 0					; compare AND res to 0
+	je .fast_return				; if a ptr is NULL, error
 
+	mov r12, rdi
 	mov rdi, rsi
-	call check_base		; check if the base is ok
+	call check_base				; check if the base is ok
 	cmp rax, 0
 	je .fast_return
 
-.fast_return:
+	mov rdi, r12
+	call skip_white_space
+	add r12, rax				; move pointer to skip whitespaces
+
+	sub rsp, 16					; allocate 2 qword on stack (one for rdi address, one for alignment)
+	mov qword [rsp + 8], r12
+
+	call get_sign
+	mov r12, [rsp + 8]			; get the changed value
+	add rsp, 16					; free space on stack
+
+	mov rdi, r12
+	mov rdx, rax
+	call get_value
+
+	.fast_return:
+	pop r12
 	pop rbp
 	ret
