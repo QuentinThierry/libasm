d39a884e08ec0990acbb41f7caf2ef8b944219fa
QuentinThierry <qthierry2@gmail.com>
2024-12-04 14:44:32 +0100

    doc: adding some comments on ft_strdup to make code easier to read

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -6,24 +6,21 @@ extern ft_strcpy
 extern malloc
 
 ft_strdup:
-	push rbp
 	push rbx				; save rbx (caller saved)
-	mov rbp, rsp
 
 	mov rbx, rdi			; save the original ptr
 	call ft_strlen			; get size of malloc
 	inc rax					; add 1 to size for \0 bit
-	mov rdi, rax
 
+	mov rdi, rax
 	call malloc				; malloc for the string size
 	cmp rax, 0
 	je exit_func			; check malloc failed
 
-	mov rdi, rax
+	mov rdi, rax			; dest=rax, src=rbx
 	mov rsi, rbx
 	call ft_strcpy			; copy the string
 
 	exit_func:
-		pop rbx
-		pop rbp
-		ret
+	pop rbx
+	ret
